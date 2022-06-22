770090a55a89d2cfb9de23dd5b2f053d13eb07c6
Kaijam Jeffers <faceless128@gmail.com>
2022-06-21 21:34:20 -0400

    added about

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Links from './components/Links';
+import './assets/css/style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Nav></Nav>
       </header>
+      <main>
+        <About></About>
+      </main>
+      <footer>
+        <Links></Links>
+      </footer>
     </div>
   );
 }
