c6ed5382a8e257961593738cc79e01eb3b4085dd
Kaijam Jeffers <faceless128@gmail.com>
2022-07-11 23:13:00 -0400

    tweaked layout

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -41,15 +41,15 @@ function Contact() {
       </div>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">Name:</label>
+          <label htmlFor="name">Name:</label><br></br>
           <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email">Email:</label><br></br>
           <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
-          <label htmlFor="message">Message:</label>
+          <label htmlFor="message">Message:</label><br></br>
           <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
         {errorMessage && (
